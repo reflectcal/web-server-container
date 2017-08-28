dd5f86f68b399f64b54798fcda18770e968d896c
alexeykomov <alexeykofficial@gmail.com>
2017-08-28 22:18:40 +0300

    edit: shorter dockerfile. edit: repo name is used.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sudo docker run -v /etc/ssl:/etc/ssl -v /etc/letsencrypt:/etc/letsencrypt \
   -v /var/www/html:/var/www/html -p 80:80 -p 3000:3000 -p 443:443 \
   -e EMAIL='mail@example.com' \
   -e DOMAIN='example.com' \
-  $1
+  reflectcal/web-server
