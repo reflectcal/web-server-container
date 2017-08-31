3fcbd5d2f9028f054f380a8518566bc91ce2dcd8
alexeykomov <alexeykofficial@gmail.com>
2017-08-31 21:57:13 +0300

    add: app server url

--- 01-install-letsencrypt.sh
+++ 01-install-letsencrypt.sh
@@ -9,6 +9,7 @@ service nginx start -d
 certbot certonly -n --webroot --webroot-path=/var/www/html -d ${DOMAIN} \
   -d www.${DOMAIN} --agree-tos --email ${EMAIL}
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.backup.conf
-sed "s/DOMAIN/${DOMAIN}/g" /etc/nginx/nginx.ssl.conf > /etc/nginx/nginx.conf
+sed "s/DOMAIN/${DOMAIN}/g" /etc/nginx/nginx.ssl.conf | \
+  sed "s/APP_SERVER_URL/${APP_SERVER_URL}/g" > /etc/nginx/nginx.conf
 service nginx reload -d
 crontab /etc/my_init.d/crontab-changed
