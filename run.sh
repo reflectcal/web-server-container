8f5dd0d612b6776b31775f919f2befe47810183d
alexeykomov <alexeykofficial@gmail.com>
2017-09-02 11:12:52 +0300

    remove: run script. add: docker compose file.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo docker run -v /etc/ssl:/etc/ssl -v /etc/letsencrypt:/etc/letsencrypt \
-  -v /var/www/html:/var/www/html -p 80:80 -p 3000:3000 -p 443:443 \
-  -e EMAIL='mail@example.com' \
-  -e DOMAIN='example.com' \
-  reflectcal/web-server
