db226f737c044e37a1a9b6a4c0957f751366b957
alexeykomov <alexeykofficial@gmail.com>
2018-01-30 23:23:31 +0300

    edit: readme, logs, renew hook

--- 01-install-letsencrypt.sh
+++ 01-install-letsencrypt.sh
@@ -5,11 +5,19 @@ if [ ! -f /etc/ssl/certs/dhparam.pem ]; then
 else
   echo "dhparam is found"
 fi
+echo "Config without SSL:"
+echo "\n"
+cat /etc/nginx/nginx.conf
+echo "Config finishes."
 service nginx start -d
 certbot certonly -n --webroot --webroot-path=/var/www/html -d ${DOMAIN} \
   -d www.${DOMAIN} --agree-tos --email ${EMAIL}
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.backup.conf
 sed "s/DOMAIN/${DOMAIN}/g" /etc/nginx/nginx.ssl.conf | \
   sed "s/APP_SERVER_URL/${APP_SERVER_URL}/g" > /etc/nginx/nginx.conf
+echo "Config with SSL:"
+echo "\n"
+cat /etc/nginx/nginx.conf
+echo "Config finishes."
 service nginx reload -d
 crontab /etc/my_init.d/crontab-changed
